02217fdb47e8bb0a46dea1aab4781bab82a0f5fc
ManonE <escoda.manon@gmail.com>
2025-07-09 12:11:39 +0200

    fetch url updated

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('https://[...].vercel.app/movies')
+    fetch('https://mymoviz-back-kappa.vercel.app/movies')
     .then(res => res.json())
     .then(data => {
       setMoviesData(data.movies);
